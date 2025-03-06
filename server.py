b22424bc1103eaf2ed4acf208aba307d0b56b6cf
soroosh185 <139605636+soroosh185@users.noreply.github.com>
2025-03-06 20:31:28 +0330

    Add files via upload

--- server.py
+++ server.py
@@ -58,4 +58,5 @@ def check_license():
 
 # اجرای سرور روی پورت 5000
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	port = int(os.environ.get("PORT", 5000)) 
+	app.run(host='0.0.0.0', port=port)
